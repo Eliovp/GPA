437aa316b160dd545b06e859531774f5a2cda85c
Chris Hesik <christopher.hesik@amd.com>
2018-03-21 17:42:34 -0400

    rtd test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,11 +115,14 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-    ],
-}
+def setup(app):
+    app.add_stylesheet('theme_overrides.css')
+
+#html_context = {
+#    'css_files': [
+#        '_static/theme_overrides.css',  # override wide tables in RTD theme
+#    ],
+#}
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
